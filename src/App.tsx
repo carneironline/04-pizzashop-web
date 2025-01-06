ff88576a7443aead861949036830b7fb39f5e15b
Rodrigo Carneiro <carneironline@gmail.com>
2025-01-06 14:54:26 -0300

    React Helmet (SEO)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routers";
 
 import "./global.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }
