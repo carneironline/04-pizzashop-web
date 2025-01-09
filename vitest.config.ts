166017f3d55304f7853f4ac1fad4314f0a652fd4
Rodrigo Carneiro <carneironline@gmail.com>
2025-01-09 17:33:03 -0300

    Utilizando Spies

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 // vitest.config.ts
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  plugins: [tsconfigPaths() as any],
   test: {
     globals: true,
     setupFiles: ["./test/setup.ts"],
