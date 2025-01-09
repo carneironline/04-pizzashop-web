0297a5d17d14f12ba3dda36bcfcbcf741cbd2952
Rodrigo Carneiro <carneironline@gmail.com>
2025-01-09 11:41:30 -0300

    Loading dos gráficos

--- src/pages/app/dashboard/chart-popular-products.tsx
+++ src/pages/app/dashboard/chart-popular-products.tsx
@@ -1,7 +1,7 @@
 import { getPopularProducts } from "@/api/get-popular-products";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useQuery } from "@tanstack/react-query";
-import { BarChart } from "lucide-react";
+import { BarChart, Loader2 } from "lucide-react";
 
 import {
   ResponsiveContainer,
@@ -43,7 +43,7 @@ export function ChartPopularProducts() {
       </CardHeader>
 
       <CardContent>
-        {popularProducts && (
+        {popularProducts ? (
           <ResponsiveContainer width="100%" height={240}>
             <PieChart style={{ fontSize: 12 }}>
               <Pie
@@ -101,6 +101,10 @@ export function ChartPopularProducts() {
               </Pie>
             </PieChart>
           </ResponsiveContainer>
+        ) : (
+          <div className="flex h-[240px] w-full items-center justify-center">
+            <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
+          </div>
         )}
       </CardContent>
     </Card>
--- src/pages/app/dashboard/chart-revenue.tsx
+++ src/pages/app/dashboard/chart-revenue.tsx
@@ -10,6 +10,7 @@ import { DateRangePicker } from "@/components/ui/date-range-picker";
 import { Label } from "@/components/ui/label";
 import { useQuery } from "@tanstack/react-query";
 import { subDays } from "date-fns";
+import { Loader2 } from "lucide-react";
 import { useMemo, useState } from "react";
 import { DateRange } from "react-day-picker";
 
@@ -73,7 +74,7 @@ export function ChartRevenue() {
       </CardHeader>
 
       <CardContent>
-        {chartData && (
+        {chartData ? (
           <ResponsiveContainer width="100%" height={240}>
             <LineChart data={chartData} style={{ fontSize: 12 }}>
               <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
@@ -96,6 +97,10 @@ export function ChartRevenue() {
               />
             </LineChart>
           </ResponsiveContainer>
+        ) : (
+          <div className="flex h-[240px] w-full items-center justify-center">
+            <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
+          </div>
         )}
       </CardContent>
     </Card>
