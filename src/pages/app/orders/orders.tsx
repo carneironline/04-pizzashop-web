63e6b9e682c4e40f8b0bee39cefacfa951f74c9f
Rodrigo Carneiro <carneironline@gmail.com>
2025-01-09 11:55:09 -0300

    Loading dos pedidos

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -13,6 +13,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getOrders } from "@/api/get-orders";
 import { useSearchParams } from "react-router-dom";
 import { z } from "zod";
+import { SkeletonOrderTable } from "./skeleton-order-table";
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -26,7 +27,7 @@ export function Orders() {
     .transform((page) => page - 1)
     .parse(searchParams.get("page") ?? "1");
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ["orders", pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -71,6 +72,8 @@ export function Orders() {
               </TableHeader>
 
               <TableBody>
+                {isLoadingOrders && <SkeletonOrderTable />}
+
                 {result &&
                   result.orders.map((order) => {
                     return <OrderTableRow key={order.orderId} order={order} />;
